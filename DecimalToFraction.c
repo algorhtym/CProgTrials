b831cae140431a36d0b7ecc814e38d1febb65ff9
M. K. KILIC <kilicmelihkemal@gmail.com>
2017-10-21 22:16:06 +0300

    Initial commit

---
+++ DecimalToFraction.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char cont;
+	do{
+	float fnumber;
+	int inumber,denom=1,i,gcd;
+
+
+	printf("Please enter the number: ");
+	scanf("%f", &fnumber);
+
+	//float mult = 10;
+
+
+	while ((int)fnumber != fnumber)
+	{
+		fnumber = fnumber*10;
+		denom = denom*10;
+		//printf("%.3f \n %d \n",fnumber,denom);
+	}
+	inumber = (int)fnumber;
+
+	printf("\n\nThe unsimplified fraction form of the number is: %d / %d \n", inumber, denom);
+
+
+	for (i=1; i<=inumber && i<=denom ; i++)
+	{
+			if(inumber%i==0 && denom%i==0)
+				gcd = i;
+	}
+
+	printf("\n\nThe greatest common divisor is : %d \n",gcd);
+
+	inumber = inumber / gcd;
+	denom = denom / gcd;
+
+	printf("\nThe simplest fraction form of the entered number is : %d / %d \n",inumber,denom);
+	printf("\n\n continue Y/N ? : ");
+
+
+	scanf(" %c",&cont);
+	system("cls");
+	}
+	while (cont=='Y' || cont=='y');
+
+
+
+
+    return 0;
+}
